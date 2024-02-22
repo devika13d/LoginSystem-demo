1abc43d69613ede10c506a08cb7cbbcf32863a6d
devika13d <devikaks606@gmail.com>
2024-02-22 20:05:29 +0530

    LoginSystemPro-Clientside using React js modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Naviga from './Naviga'
+import './home.css'
+import im from '../components/react.png'
 function Home() {
   return (
     <div>
       <Naviga/>
+      <div>
+        <img src={im} alt="" />
+      </div>
 
     </div>
   )
